85a81a0d8f70e65faab54ff77745a7bf13993a2c
vinetha24 <113199878+vinetha24@users.noreply.github.com>
2024-08-28 21:49:59 +0530

    Update Main.java
    Third commit

--- vitals/Main.java
+++ vitals/Main.java
@@ -1,28 +1,14 @@
 package vitals;
 
-
-
 public class Main {
-
-    static boolean checkTemperature(float temperature){
-    return temperature < 0 || temperature > 45;
-}
-
-static boolean checkState(float soc){
-    return soc < 20 || soc > 80;
-}
-
-static boolean checkChargeRate(float chargeRate){
-    return chargeRate > 0.8;
-}
     static boolean batteryIsOk(float temperature, float soc, float chargeRate) {
-        if(checkTemperature(temperature)) {
+        if(temperature < 0 || temperature > 45) {
             System.out.println("Temperature is out of range!");
             return false;
-        } else if(checkState(soc)) {
+        } else if(soc < 20 || soc > 80) {
             System.out.println("State of Charge is out of range!");
             return false;
-        } else if(checkChargeRatechargeRate()) {
+        } else if(chargeRate > 0.8) {
             System.out.println("Charge Rate is out of range!");
             return false;
         }
